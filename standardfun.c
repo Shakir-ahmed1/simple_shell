642f290b311db670e619c5ac9d1681083937a830
amlaksil <silamlakdesye@gmail.com>
2023-02-20 16:18:04 +0300

    Add standard function

---
+++ standardfun.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @dest: first string.
+ * @src: second string.
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
+ */
+int _strcmp(char *dest, char *src)
+{
+	int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(dest + i) == '\0') && (*(src + i) == '\0'))
+			break;
+		op = *(dest + i) - *(src + i);
+		i++;
+	}
+
+	return (op);
+}
